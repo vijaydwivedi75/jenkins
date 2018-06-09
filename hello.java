cc8a1993bbe073eaeb2d0f5226b6936bab11776b
Vijay Prakash Dwivedi <vijaydwivedi75@gmail.com>
2018-06-10 01:41:21 +0530

    added a demo hello program

---
+++ hello.java
@@ -0,0 +1,7 @@
+public class hello{
+    public static void main(){
+        for(int i=1; i <= 10; i++){
+            System.out.println("Hello World ... " + i);
+        }
+    }
+}
---
+++ hello.py
@@ -0,0 +1,2 @@
+for i in range(10):
+    print("Hello World ... %d " % (i+1))
